76902a6f83b015a392b38b78f5e0631f451f7060
Lewis Bull <lewis@verve-homecare.co.uk>
2025-06-23 15:16:18 +0100

    Fixed views.py error

--- drf_api/views.py
+++ drf_api/views.py
@@ -1,14 +1,17 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from settings import (
-    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE, JWT_AUTH_SECURE
+from .settings import (
+    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
+    JWT_AUTH_SECURE,
 )
+
 
 @api_view()
 def root_route(request):
     return Response({
-        "Message": "Welcome to the Pawlio API!"
+        "message": "Welcome to my drf API!"
     })
+
 
 # dj-rest-auth logout view fix
 @api_view(['POST'])
