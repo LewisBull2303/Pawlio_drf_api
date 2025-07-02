c7665c494641533b725fde7a35c6d8d275ea3a8a
Lewis Bull <lewis@verve-homecare.co.uk>
2025-07-02 10:22:54 +0100

    Updated the posts componant

--- posts/views.py
+++ posts/views.py
@@ -7,10 +7,6 @@ from .serializers import PostSerializer
 
 
 class PostList(generics.ListCreateAPIView):
-    """
-    List posts or create a post if logged in
-    The perform_create method associates the post with the logged in user.
-    """
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Post.objects.annotate(
@@ -20,12 +16,12 @@ class PostList(generics.ListCreateAPIView):
     filter_backends = [
         filters.OrderingFilter,
         filters.SearchFilter,
-        DjangoFilterBackend
+        DjangoFilterBackend,
     ]
     filterset_fields = [
         'owner__followed__owner__profile',
         'likes__owner__profile',
-        'owner__profile'
+        'owner__profile',
     ]
     search_fields = [
         'owner__username',
@@ -42,9 +38,6 @@ class PostList(generics.ListCreateAPIView):
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    """
-    Retrieve a post and edit or delete it if you own it.
-    """
     serializer_class = PostSerializer
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Post.objects.annotate(
