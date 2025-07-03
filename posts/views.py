20e0501cab6905bff973d0b8be1a3337ffeec93c
Lewis Bull <lewis@verve-homecare.co.uk>
2025-07-03 12:35:27 +0100

    Updated perform create in views

--- posts/views.py
+++ posts/views.py
@@ -44,7 +44,7 @@ class PostList(generics.ListCreateAPIView):
     ]
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+        serializer.save()
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
