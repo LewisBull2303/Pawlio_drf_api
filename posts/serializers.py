a228dd85dfc2adaa34b05f6fcb45ead44865b39d
Lewis Bull <lewis@verve-homecare.co.uk>
2025-07-08 11:44:51 +0100

    Updated posts serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -4,6 +4,9 @@ from likes.models import Like
 
 
 class PostSerializer(serializers.ModelSerializer):
+    """
+    A class for a PostSerializer
+    """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
@@ -13,23 +16,26 @@ class PostSerializer(serializers.ModelSerializer):
     likes_count = serializers.ReadOnlyField()
 
     def validate_image(self, value):
-        # Check image height, width and size
-        from PIL import Image
-        import io
+        """
+        Validation of the uploaded image size
+        """
+        # Image height limit of 4096 px
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Your image exceeds the height limit of 4096px.'
+            )
 
-        # Open the image file
-        image = Image.open(value)
+        # Image width limit of 4096 px
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Your image exceeds the width limit of 4096px.'
+            )
 
-        if image.height > 4096:
-            raise serializers.ValidationError('Your image exceeds the height limit of 4096px.')
-
-        if image.width > 4096:
-            raise serializers.ValidationError('Your image exceeds the width limit of 4096px.')
-
-        # File size in bytes
+        # Image size limit of 2 megabytes
         if value.size > 1024 * 1024 * 2:
-            raise serializers.ValidationError('Your image is too large. Max size is 2MB.')
-
+            raise serializers.ValidationError(
+                'Your image is too large. Max size is 2MB.'
+            )
         return value
 
     def get_is_owner(self, obj):
@@ -44,19 +50,6 @@ class PostSerializer(serializers.ModelSerializer):
             ).first()
             return like.id if like else None
         return None
-    
-    def create(self, validated_data):
-        request = self.context.get('request')
-        validated_data['owner'] = request.user
-
-        image = validated_data.pop('image', None)
-        post = Post.objects.create(**validated_data)
-
-        if image:
-            post.image = image
-            post.save()
-
-        return post
 
     class Meta:
         model = Post
