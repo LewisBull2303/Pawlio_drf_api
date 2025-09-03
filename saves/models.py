f9095049603b66a89f289352ec2a212bdf378e6e
Lewis Bull <lewisb@aldingbourne.org>
2025-09-03 16:00:30 +0100

    Fixed a bug with the variable names

--- saves/models.py
+++ saves/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Saves(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey('posts.Post', on_delete=models.CASCADE)
     saved_at = models.DateTimeField(auto_now_add=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        unique_together = ('user', 'post')
+        unique_together = ('owner', 'post')
         ordering = ['-saved_at']
 
     def __str__(self):
-        return f'{self.user.username} saved {self.post.title}'
+        return f'{self.owner.username} saved {self.post.title}'
