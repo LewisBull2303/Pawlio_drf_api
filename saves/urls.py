c2cd3d0422c79a5ca01bf5441640f691e86bd1d8
Lewis Bull <lewisb@aldingbourne.org>
2025-09-03 11:18:46 +0100

    Removed an unnecessary line from the urls

--- saves/urls.py
+++ saves/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from saves import views
 
 urlpatterns = [
-    path('saves/', views.SaveList.as_view()),
-    path('saves/<int:pk>/', views.SaveDetail.as_view())]
+    path('saves/', views.SaveList.as_view())
+    ]
