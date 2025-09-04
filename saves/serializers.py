3d3a6425e24043dd4acf4639580ac24265d5c6ea
Lewis Bull <lewisb@aldingbourne.org>
2025-09-04 15:22:21 +0100

    Attempting to fix the unauthorised errors when saving

--- saves/serializers.py
+++ saves/serializers.py
@@ -1,30 +1,21 @@
+from posts.models import Post
 from rest_framework import serializers
 from .models import Saves
-from posts.models import Post
 
 class SaveSerializer(serializers.ModelSerializer):
-    """
-    A class for a SaveSerializer
-    """
     owner = serializers.ReadOnlyField(source='owner.username')
+    post = serializers.PrimaryKeyRelatedField(queryset=Post.objects.all())  # <- add this
     post_id = serializers.ReadOnlyField(source='post.id')
     post_image = serializers.ReadOnlyField(source='post.image.url')
     post_owner = serializers.ReadOnlyField(source='post.owner.username')
     created_at = serializers.ReadOnlyField()
-
-    def get_post_details(self, obj):
-        post = Post.objects.get(id=obj.post.id)
-        return {
-            'id': post.id,
-            'image': post.image.url,
-            'owner': post.owner.username,
-        }
 
     class Meta:
         model = Saves
         fields = [
             'id',
             'owner',
+            'post',
             'post_id',
             'post_image',
             'post_owner',
