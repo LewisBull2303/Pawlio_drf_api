5abb849e77c5d47cba3086f3907e000603742d1d
Lewis Bull <lewisb@aldingbourne.org>
2025-09-05 12:11:44 +0100

    Forgot to save the model of saves into the commit

--- saves/models.py
+++ saves/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
 
-# Create your models here.
+
+class Save(models.Model):
+    """
+    Save model, related to 'owner' and 'post'.
+    'owner' is a User instance and 'post' is a Post instance.
+    'unique_together' ensures a user can't save the same post more than once.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post, related_name='saves', on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['owner', 'post']
+
+    def __str__(self):
+        return f'{self.owner} saved {self.post}'
