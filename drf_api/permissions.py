6e38e995c9b9816feb9605c69e9180dac52eb413
Lewis Bull <lewisb@aldingbourne.org>
2025-09-09 12:25:18 +0100

    Changed all of the drf_api files to be PEP8 Compliant

--- drf_api/permissions.py
+++ drf_api/permissions.py
@@ -1,4 +1,8 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from rest_framework import permissions
+
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
